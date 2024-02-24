89a2195252e461f7ff5961fdc8580109b39eb651
Quba456555 <144456270+Quba456555@users.noreply.github.com>
2024-02-24 15:43:17 +0100

    Update CZOLG_produkcja.py

--- CZOLG_produkcja.py
+++ CZOLG_produkcja.py
@@ -4,4 +4,21 @@ class Czolg:
     self.numer_seryjny=numer_seryjny
     self.cena=cena
 
+class Dzialo(Czolg):
+    def __init__(self,nazwa,numer_seryjny,cena,kaliber):
+        super().__init__(nazwa,numer_seryjny,cena)
+        self.kaliber=kaliber
+
+    def edytuj_kaliber(self,nowy_kaliber):
+        self.klaiber=nowy_kaliber
+
+class Silnik(Czolg):
+    def __init___(self,nazwa,numer_seryjny,cena,moc):
+        super().__init__(nazwa,numer_seryjny,cena)
+        self.moc=moc
+
+    def edytuj_moc(self,nowa_moc):
+        self.moc=nowa_moc
+#na razie wystarczy
+#===========================================================
 
