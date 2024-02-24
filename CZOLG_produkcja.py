1f723750faec7ba05de0103fd755d1c1c7b6fb13
Quba456555 <144456270+Quba456555@users.noreply.github.com>
2024-02-24 16:32:20 +0100

    Update CZOLG_produkcja.py

--- CZOLG_produkcja.py
+++ CZOLG_produkcja.py
@@ -1,29 +1,74 @@
-class Czolg:
-  def __init__(self,dzialo,silnik)
-    self.dzialo = dzialo
-    self.silnik = silnik
+class Czolg():
+    def __init__(self) -> None:
+        self.cena
+        self.numer_seryjny
+        
+    
+    def zmien1(self,nowa_cena):
+        self.cena = nowa_cena
 
-class elementy_czolgu:
-  def __init__(self,nazwa,numer_seryjny,cena):
-    self.nazwa=nazwa
-    self.numer_seryjny=numer_seryjny
-    self.cena=cena
+    def inf1(self):
+        print(self.cena)
 
-class Dzialo(elementy_czolgu):
-    def __init__(self,nazwa,numer_seryjny,cena,kaliber):
-        super().__init__(nazwa,numer_seryjny,cena)
-        self.kaliber=kaliber
+class Dzialo():
+    def __init__(self) -> None:
+        self.kaliber = kaliber
+    
+    def zmien2(self, nowy_kaliber):
+        self.kaliber = nowy_kaliber
 
-    def edytuj_kaliber(self,nowy_kaliber):
-        self.klaiber=nowy_kaliber
+    def inf2(self):
+        print(self.kaliber)
 
-class Silnik(elementy_czolgu):
-    def __init___(self,nazwa,numer_seryjny,cena,moc):
-        super().__init__(nazwa,numer_seryjny,cena)
-        self.moc=moc
+class Element3:
+    def __init__(self) -> None:
+        self.cecha3 = 1
+    
+    def zmien3(self):
+        self.cecha3 = float(input())
 
-    def edytuj_moc(self,nowa_moc):
-        self.moc=nowa_moc
-#na razie wystarczy
-#===========================================================
+    def inf3(self):
+        print(self.cecha3)
 
+class Element4:
+    def __init__(self) -> None:
+        self.cecha3 = 1
+    
+    def zmien3(self):
+        self.cecha3 = float(input())
+
+    def inf3(self):
+        print(self.cecha3)
+
+class Element4:
+    def __init__(self) -> None:
+        self.cecha3 = 1
+    
+    def zmien3(self):
+        self.cecha3 = float(input())
+
+    def inf3(self):
+        print(self.cecha3)
+
+
+class Calosc():                 #nie moznadziedziczyc wiecej niz jednje classy 
+    def __init__(self):         #ale za ro mozna zapisac je jako obiekty
+        self.elm1 = Element1()
+        self.elm2 = Element2()
+        self.elm3 = Element3()
+    
+    def inf(self):
+        self.elm1.inf1()
+        self.elm2.inf2()
+        self.elm3.inf3()
+    
+    def zmien(self):
+        self.elm1.zmien1()
+        self.elm2.zmien2()
+        self.elm3.zmien3()
+    
+x = Calosc()
+
+# x.inf()
+# x.zmien()
+# x.inf()
